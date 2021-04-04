072e75ea03143f2c744cd01f99e790bcb937b37b
Bahchaev <wbahas@mail.ru>
2021-04-04 19:58:14 +0700

    Очистил код от неиспользующихся данных

--- src/Components/CategoryTreeBranchNode/index.js
+++ src/Components/CategoryTreeBranchNode/index.js
@@ -4,35 +4,26 @@ import styles from "./styles.module.css"
 export default function CategoryTreeBranchNode({imageSrc}) {
 
     const [isShown, setIsShown] = useState(false);
-    const [image, setImage] = useState(null); //данные, полученные с сервера
-    const [isLoaded, setIsLoaded] = useState(true); // статус загрузки данных с сервера
-    const [error, setError] = useState(null); // ошибки при загрузке данных с сервера
 
     const handleClick = () => {
         setIsShown(!isShown)
     };
 
-    if (error) {
-        return <div>{error.message}</div>
-    } else if (!isLoaded) {
-        return <div>Загрузка...</div>
-    } else {
-        return (
-            <div>
-                <img
-                    className={styles.thumb}
-                    src={`http://contest.elecard.ru/frontend_data/${imageSrc}`}
-                    alt=""
-                    onClick={handleClick}
-                />
-                <img
-                    className={isShown ? styles.largeImage : styles.hidden}
-                    src={`http://contest.elecard.ru/frontend_data/${imageSrc}`}
-                    alt=""
-                />
-                <div className={isShown ? styles.wrapper : styles.hidden} onClick={handleClick}/>
-            </div>
+    return (
+        <div>
+            <img
+                className={styles.thumb}
+                src={`http://contest.elecard.ru/frontend_data/${imageSrc}`}
+                alt=""
+                onClick={handleClick}
+            />
+            <img
+                className={isShown ? styles.largeImage : styles.hidden}
+                src={`http://contest.elecard.ru/frontend_data/${imageSrc}`}
+                alt=""
+            />
+            <div className={isShown ? styles.wrapper : styles.hidden} onClick={handleClick}/>
+        </div>
 
-        )
-    }
+    )
 }
